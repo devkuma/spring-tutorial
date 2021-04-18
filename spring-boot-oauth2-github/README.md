51740e65caad23de5a0953c4b25cf465f6ab8e3a
kimkc <redfreek2c@gmail.com>
2021-04-19 07:25:17 +0900

    add spring-boot-oauth2-github

---
+++ spring-boot-oauth2-github/README.md
@@ -0,0 +1,6 @@
+# spring-boot-oauth2-github
+github를 이용한 oauth2 구현
+
+## 참고
+- https://spring.io/guides/tutorials/spring-boot-oauth2/
+- https://jasunhee.tistory.com/27
---
+++ spring-boot-oauth2-github/src/test/java/com/devkuma/oauth2/github/SpringBootOauth2GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkuma.oauth2.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
