2f850f1c4f60fced3e445e58cd2acde0c6897ad3
김경철 <kimkc@wemakeprice.com>
2022-08-13 09:17:09 +0900

    refactoring

--- spring-data-elasticsearch-2/README.md
+++
@@ -1,4 +0,0 @@
-# Spring Data Elasticsearch and Kotlin
-
-이 프로젝트의 설명은 아래 링크를 참고 바란다.
-- https://devkuma.github.io/Kotlin/Spring-Data-Elasticsearch-Kotlin/
--- spring-data-elasticsearch-2/src/main/kotlin/com/devkuma/elasticsearch/repository/ElasticsearchRepository.kt
+++
@@ -1,6 +0,0 @@
-//package com.devkuma.elasticsearch.repository
-//
-//import com.devkuma.elasticsearch.document.Phone
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
-//
-//interface ElasticsearchRepository: ElasticsearchRepository<Phone, Int>
