90765fc7d6f10cdda79bfded6cc015166b0c7cd3
kimkc <kimkc@linecorp.com>
2022-12-09 10:25:51 +0900

    add spring-thymeleaf

---
+++ spring-thymeleaf/spring-thymeleaf/src/test/java/com/devkuma/thymeleaf/SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkuma.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
