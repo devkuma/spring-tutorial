fb9f63bab485e24d7adf0a2542fb19c99c45d885
kimkc <kimkc@linecorp.com>
2022-12-10 04:36:12 +0900

    add spring-actuator-prometheus

---
+++ spring-actuator-prometheus/src/test/java/com/devkuma/prometheus/ActuatorPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkuma.prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
